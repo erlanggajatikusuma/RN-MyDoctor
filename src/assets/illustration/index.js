6878b274e87c2a25c803d99d60c95e72642b2f05
kusuma <erlanggajatikusuma007@gmail.com>
2021-03-02 18:42:23 +0700

    doctor screen section

--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -1,5 +1,15 @@
 import ILLogo from './logo.svg';
 import ILGetStarted from './get-started.png';
 import ILNullPhoto from './null-photo.png';
+import ILCatUmum from './cat-doctor-umum.svg';
+import ILCatPsikiater from './cat-doctor-psikiater.svg';
+import ILCatObat from './cat-doctor-obat.svg';
 
-export {ILLogo, ILGetStarted, ILNullPhoto};
+export {
+  ILLogo,
+  ILGetStarted,
+  ILNullPhoto,
+  ILCatObat,
+  ILCatUmum,
+  ILCatPsikiater,
+};
