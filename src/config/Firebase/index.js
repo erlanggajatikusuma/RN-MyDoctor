622b8f53b8844d3c57ae3d9ff246f09a078dc960
kusuma <erlanggajatikusuma007@gmail.com>
2021-03-03 15:10:58 +0700

    Setup firebase

---
+++ src/config/Firebase/index.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDnOfqeQyquJ7Jaij2ftNsK9mAA1OAAe9U',
+  authDomain: 'my-doctor-11.firebaseapp.com',
+  projectId: 'my-doctor-11',
+  storageBucket: 'my-doctor-11.appspot.com',
+  messagingSenderId: '210640200319',
+  appId: '1:210640200319:web:30320aac8233a20728954a',
+});
+
+const Firebase = firebase;
+export default Firebase;
