49b49d363f51347ea955482bc3efb9ca202ec877
kusuma <erlanggajatikusuma007@gmail.com>
2021-03-04 09:07:15 +0700

    update Photo & store to localStorage

--- src/pages/UpdateProfile/index.js
+++ src/pages/UpdateProfile/index.js
@@ -1,28 +1,111 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
 import {Button, Gap, Header, Input, Profile} from '../../components';
-import {colors} from '../../utils';
+import {colors, getData, storeData} from '../../utils';
+import {Firebase} from '../../config';
+import {showMessage} from 'react-native-flash-message';
+import {launchImageLibrary} from 'react-native-image-picker';
+import {ILNullPhoto} from '../../assets';
 
 const UpdateProfile = ({navigation}) => {
+  const [profile, setProfile] = useState({
+    fullName: '',
+    profession: '',
+    email: '',
+  });
+
+  const [password, setPassword] = useState('');
+  const [photo, setPhoto] = useState(ILNullPhoto);
+  const [photoDB, setPhotoDB] = useState('');
+
+  useEffect(() => {
+    getData('user').then((res) => {
+      const data = res;
+      setPhoto({uri: res.photo});
+      setProfile(data);
+    });
+  }, []);
+
+  const updateProfile = () => {
+    console.log('Profile: ', profile);
+    const data = profile;
+    data.photo = photoDB;
+
+    Firebase.database()
+      .ref(`users/${profile.uid}/`)
+      .update(data)
+      .then(() => {
+        console.log('Success', data);
+        storeData('user', data);
+      })
+      .catch((error) => {
+        showMessage({
+          message: error.message,
+          type: 'default',
+          backgroundColor: colors.error,
+          color: colors.white,
+        });
+      });
+    // navigation.goBack('UserProfile')
+  };
+
+  const changeText = (key, value) => {
+    setProfile({
+      ...profile,
+      [key]: value,
+    });
+  };
+
+  const getImage = () => {
+    launchImageLibrary(
+      {
+        mediaType: 'photo',
+        quality: 0.5,
+        maxHeight: 250,
+        maxWidth: 250,
+        includeBase64: true,
+      },
+      (response) => {
+        if (response.didCancel || response.error) {
+          showMessage({
+            message: 'Oops, sepertinya anda tidak memilih fotonya',
+            type: 'default',
+            backgroundColor: colors.error,
+            color: colors.white,
+          });
+        } else {
+          const base64Photo = `data:${response.type};base64, ${response.base64}`;
+          const source = {uri: response.uri};
+          setPhotoDB(base64Photo);
+          setPhoto(source);
+        }
+      },
+    );
+  };
   return (
     <View style={styles.page}>
       <Header title="Edit Profile" onPress={() => navigation.goBack()} />
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.content}>
-          <Profile isRemove />
+          <Profile isRemove photo={photo} onPress={getImage} />
           <Gap height={26} />
-          <Input label="Full Name" />
-          <Gap height={24} />
-          <Input label="Pekerjaan" />
-          <Gap height={24} />
-          <Input label="Email" />
-          <Gap height={24} />
-          <Input label="Password" />
-          <Gap height={40} />
-          <Button
-            title="Save Profile"
-            onPress={() => navigation.goBack('UserProfile')}
+          <Input
+            label="Full Name"
+            value={profile.fullName}
+            onChangeText={(value) => changeText('fullName', value)}
           />
+          <Gap height={24} />
+          <Input
+            label="Pekerjaan"
+            value={profile.profession}
+            onChangeText={(value) => changeText('profession', value)}
+          />
+          <Gap height={24} />
+          <Input label="Email" value={profile.email} disable />
+          <Gap height={24} />
+          <Input label="Password" value={password} />
+          <Gap height={40} />
+          <Button title="Save Profile" onPress={updateProfile} />
         </View>
       </ScrollView>
     </View>
