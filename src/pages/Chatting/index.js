f610eb31b963c0d91430eed51afb1ad18cec5a1f
kusuma <erlanggajatikusuma007@gmail.com>
2021-03-05 08:11:50 +0700

    Integrasi chatting data ke firebase

--- src/pages/Chatting/index.js
+++ src/pages/Chatting/index.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {ChatItem, Header, InputChat} from '../../components';
-import {colors, fonts} from '../../utils';
+import {colors, fonts, getData, showError} from '../../utils';
+import {Firebase} from '../../config';
 
 const Chatting = ({navigation, route}) => {
   const dataDoctor = route.params;
+  const [chatContent, setChatContent] = useState('');
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    getData('user').then((res) => {
+      setUser(res);
+    });
+  }, []);
+
+  const chatSend = () => {
+    const today = new Date();
+    const hour = today.getHours();
+    const minutes = today.getMinutes();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const date = today.getDate();
+
+    const data = {
+      sendBy: user.uid,
+      chatDate: new Date().getTime(),
+      chatTime: `${hour}:${minutes} ${hour > 12 ? 'PM' : 'AM'}`,
+      chatContent: chatContent,
+    };
+
+    Firebase.database()
+      .ref(
+        `chatting/${user.uid}_${dataDoctor.data.uid}/allChat/${year}-${month}-${date}`,
+      )
+      .push(data)
+      .then(() => {
+        setChatContent('');
+      })
+      .catch((error) => showError(error.message));
+  };
+
   return (
     <View style={styles.page}>
       <Header
@@ -23,9 +59,9 @@ const Chatting = ({navigation, route}) => {
         </ScrollView>
       </View>
       <InputChat
-        value="h"
-        onChangeText={() => alert('Change text')}
-        onButtonPress={() => alert('Button press')}
+        value={chatContent}
+        onChangeText={(value) => setChatContent(value)}
+        onButtonPress={chatSend}
       />
     </View>
   );
